a87d8110aeff1d2dba5800d6e0ac6a36e552ff99
shinushaju <shinushaju.ac@gmail.com>
2020-09-02 22:50:48 +0530

    added express server

--- server/app.js
+++ server/app.js
@@ -2,5 +2,11 @@ const express = require('express')
 const app = express()
  
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
+
+
+const port = 8080;
+app.listen(port, () => {
+    console.log('Node API is listening on port: %d', port);
+});
