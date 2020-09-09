a04a57ed5badc8bc970dba33399a0498671cd079
shinushaju <shinushaju.ac@gmail.com>
2020-09-09 22:40:20 +0530

    added virtual field and methods for user data security

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const { v1: uuidv1 } = require('uuid');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,7 +24,37 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     },
     updated_on: Date
-
 });
+
+// virtual field
+userSchema.virtual('password')
+
+    .set((password) => {
+        // create temporary variable called _password
+        this._password = password
+        // generate a timestamp
+        this.salt = uuidv1()
+        // encryptPassword
+        this.hashed_password = this.encryptPassword(password)
+    })
+
+    .get(() => {
+        return this._password
+    })
+
+// methods
+userSchema.methods = {
+    encryptPassword: (password) => {
+            if(!password) return "";
+            try {
+                return crypto.createHmac('sha256', this.salt)
+                            .update(password)
+                            .digest('hex');
+            }
+            catch{
+                return "";
+            }
+    }
+}
 
 module.exports = mongoose.model("User", userSchema)
