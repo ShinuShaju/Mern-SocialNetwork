55c6ee80f4da71a809f918997156becde4672eee
shinushaju <shinushaju.ac@gmail.com>
2020-09-09 21:28:43 +0530

    get posts from DB - selected id title body

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -4,6 +4,7 @@ const Post = require('../models/post')
 exports.getPosts = (req, res) => {
 
     const posts = Post.find()
+    .select("_id title body")
         .then((posts) => {
                 res.json({ posts });
         })
