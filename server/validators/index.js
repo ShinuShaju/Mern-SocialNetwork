3d9dd50ca4e43cc552755c51ed726abd92a524ce
shinushaju <shinushaju.ac@gmail.com>
2020-10-05 21:52:35 +0530

    validated user signup

--- server/validators/index.js
+++ server/validators/index.js
@@ -25,3 +25,34 @@ exports.createPostValidator = (req, res, next) => {
     next();
 
 };
+
+exports.userSignupValidator = (req, res, next) => {
+
+    req.check("name", "Name cannot be empty!").notEmpty();
+    req.check("email", "Email must be between 3 to 32 characters!")
+    .matches(/.+\@.+\..+/)
+    .withMessage("Email must contain @")
+    .isLength({
+        min:3,
+        max: 32
+    });
+
+    req.check("password", "Password is required!").notEmpty();
+    req.check("password")
+    .isLength({
+        min:6
+    })
+    .withMessage("Password must contain minimum 6 characters!")
+    .matches(/\d/)
+    .withMessage("Password must contain a number!");
+
+    // check for errors
+    const errors = req.validationErrors();
+
+    if (errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).json({ error: firstError });
+    }
+
+    next();
+}
