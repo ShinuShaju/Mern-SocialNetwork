ce886c2a0f6dba8488c01084886cf26c0f1f5852
shinushaju <shinushaju.ac@gmail.com>
2020-10-05 20:51:09 +0530

    updated auth status

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,14 +7,14 @@ exports.signup = async (req, res) => {
 
     if(userExists) 
         return res.status(403).json({
-            error: 'This email id is taken!'
+            error: 'Email already exists!'
         });
 
     const user = await new User(req.body);
     
     await user.save();
 
-    res.status(200).json({ user });
+    res.status(200).json({ message: "Signup Success! Please Login."});
 
 
 }
