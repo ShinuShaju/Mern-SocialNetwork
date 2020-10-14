006b51fa9c841af629538abbaac26f007338bf76
shinushaju <shinushaju.ac@gmail.com>
2020-10-15 00:40:08 +0530

    added middleware for api documentation

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const fs = require('fs')
 const expressValidator = require('express-validator')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
@@ -26,9 +27,22 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+
 app.use('/', postRoutes);
 app.use('/', authRoutes);
 app.use('/', userRoutes);
+
+app.get('/', (req, res) => {
+  fs.readFile('docs/apiDocs.json', (err, data) => {
+    if(err) {
+      res.status(400).json ({
+        error: err
+      })
+    }
+    const docs = JSON.parse(data)
+    res.json(docs)
+  })
+})
 
 app.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
