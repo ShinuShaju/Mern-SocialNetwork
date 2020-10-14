08d3a71991284d87fe6da42543ae6e905d8917ea
shinushaju <shinushaju.ac@gmail.com>
2020-10-15 00:13:02 +0530

    added route to update a post

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getPosts, createPost, postsByUser, postById, deletePost, isPoster } = require('../controllers/post');
+const { getPosts, createPost, postsByUser, postById, deletePost, updatePost, isPoster } = require('../controllers/post');
 const { requireSignin } = require('../controllers/auth');
 const { userById } = require('../controllers/user')
 const { createPostValidator } = require('../validators');
@@ -7,14 +7,15 @@ const { createPostValidator } = require('../validators');
 const router = express.Router()
 
 router.get('/',getPosts)
-router.post('/post/new/:userId', requireSignin, createPost, createPostValidator)
-router.get('/posts/by/:userId',requireSignin, postsByUser)
+router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
+router.get('/posts/by/:userId',requireSignin, postsByUser);
+router.put('/post/:postId', requireSignin, isPoster, updatePost);
 router.delete('/post/:postId', requireSignin, isPoster, deletePost);
 
 // any route with userId, our app first executes userById()
-router.param("userId", userById)
+router.param("userId", userById);
 // any route with postId, our app first executes postById()
-router.param("postId", postById)
+router.param("postId", postById);
 
 module.exports = router;
 
